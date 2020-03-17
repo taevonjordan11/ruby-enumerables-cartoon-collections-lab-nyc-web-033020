401d0cc2b883bb2dbfb35e0c49d2cae74e8b7813
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 19:21:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,9 @@ def summon_captain_planet(veggies)
   end
 end
 
-def long_planeteer_calls(long_planeteer_calls)
+def long_planeteer_calls(long_calls)
   answer = false
-  long_planteer_calls.each do |call|
+  long_calls.each do |call|
     if call.length > 4
       answer = true
     end
