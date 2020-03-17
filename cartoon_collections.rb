9b1ab66906814789cf72ab51ba0d384cadab1a74
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 19:19:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,13 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
 def find_the_cheese# code an argument here
